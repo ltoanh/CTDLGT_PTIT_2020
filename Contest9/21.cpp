897c716b87eed16201098b6e99d3218e7b9efb83
Le Tu Oanh <ltoanh.laxy@gmail.com>
2020-06-07 15:58:39 +0700

    update

--- Contest9/21.cpp
+++ Contest9/21.cpp
@@ -20,14 +20,15 @@ bool isVisitedAll(bool vis[]){
 	return 1;
 }
 
-void dfs(int u, vi a[],bool vis[]){
+void dfs(int u, vi a[],bool vis[], ii pos){
 	stack<int> s;
 	s.push(u); vis[u]=1;
 	while(!s.empty()){
 		int ans=s.top(); s.pop();
 		for(int i=0; i<a[ans].size(); ++i){
 			int t1=a[ans][i];
-			if(!vis[t1]){
+			ii check; check.F=ans, check.S=t1;
+			if(!vis[t1] && check!=pos){
 				vis[t1]=1;
 				s.push(ans); s.push(t1);
 				break;
@@ -38,17 +39,15 @@ void dfs(int u, vi a[],bool vis[]){
 
 void countEdge(bool vis[], vi a[]){
 	for(int i=1; i<=n; ++i){
-		for(int j=i+1; j<=n; ++j){
-			for(int i=1; i<=n; ++i) vis[i]=0;
-	 		int pos=find(a[i].begin(), a[i].end(), j)-a[i].begin();
-	 		int val=a[i][pos], fl=0;
-			if(pos!=a[i].size()){
-	 			a[i].erase(a[i].begin()+pos);
-	 			fl=1;
+		for(int j=0; j<a[i].size(); ++j){
+			if(i<a[i][j]){
+				for(int i=1; i<=n; ++i) vis[i]=0;
+		 		ii pos; pos.F=i, pos.S=a[i][j];
+				dfs(1, a, vis, pos);
+				if(!isVisitedAll(vis)){
+					cout<<i<<" "<<a[i][j]<<" ";
+				}
 			}
-			dfs(1, a, vis);
-			if(!isVisitedAll(vis)) cout<<i<<" "<<j<<" ";	
-			if(fl) a[i].insert(a[i].begin()+pos, val);
 		}
 	}
 	cout<<endl;
